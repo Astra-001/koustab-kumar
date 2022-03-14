ef5a6713f2095eee99c7c5085e0e8d9ce4428cb3
Astra-001 <60982780+Astra-001@users.noreply.github.com>
2022-03-15 01:25:00 +0530

    First Commite
    nothing extra

---
+++ unikorn/assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $('#logo-bar').click(function(){
+        $('.menu-items').toggle('fast','linear');
+    });
+    $('.cancel').click(function(){
+        $('.menu-items').hide('fast','linear');
+    });
+
+    // if($('.menu-items'))
+
+
+
+});
